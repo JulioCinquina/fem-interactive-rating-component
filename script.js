c2788174a6c0b31caac46eba05cc32341487c724
Julio Cinquina <66167767+JulioCinquina@users.noreply.github.com>
2022-09-05 19:49:49 -0300

    Add form handler script

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const formEl = document.querySelector('.form');
+const surveyEl = document.querySelector('.survey');
+const thanksEl = document.querySelector('.thanks');
+const ratingEl = thanksEl.querySelector('.rating');
+
+const displayThanks = function () {
+  surveyEl.style.display = 'none';
+  thanksEl.style.display = 'grid';
+};
+
+const handleSubmit = function (event) {
+  const selectedRating = formEl.querySelector('input:checked').value;
+  ratingEl.textContent = `You selected ${selectedRating} out of 5`;
+  displayThanks();
+  event.preventDefault();
+};
+
+formEl.addEventListener('submit', handleSubmit);
